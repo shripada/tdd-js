396066979039e4ef4043c73ae11c3eb6617eed25
shripada <shripada.hebbar@codecraft.co.in>
2023-09-01 15:06:23 +0530

    feat: added few tests to demo the deleting and shallow copying of objects

--- src/8.objects.test.js
+++ src/8.objects.test.js
@@ -24,7 +24,7 @@ test('object creation using literal syntax', () => {
   expect(cricket[10]).toBe('Ten!') // number gets coersed to a string. Array are objects where keys are indices.
 
   // ❌ trying to access non existent keys will return undefined
-  expect(cricket.nonExistent).toBeUndefined()
+  expect(cricket.nonExistent).toBeUndefined() //undefined
 
   //➕ Setting a value to a non existent property will end up introducing that property into object.
   expect(cricket.worldCupWinners).toBeUndefined()
@@ -43,7 +43,7 @@ test('object creation using literal syntax', () => {
       ['supports fp']: true, // key can be an expression that resolves to a string, if expression involved need to use []
       usages: ['frontend-web', 'node js', 'mobile-react-native'],
     },
-    description: function () {
+    description() {
       // value is a function, a function that is a member of an object is known as method
       return `inventor: ${this.inventor}` //method can access other prop of of objects using a special arg passed to it called this.
     },
@@ -54,6 +54,7 @@ test('object creation using literal syntax', () => {
     'node js',
     'mobile-react-native',
   ])
+
   // trying to access a property on an undefined object is going to throw an exception
   expect(javaScriptHistory.nonExistent).toBeUndefined()
   // 🐞 try to access a prop from nonExistent will throw an exception
@@ -81,7 +82,7 @@ test('Creating objects with new', () => {
   let o = new Object() // Create an empty object: same as {}.
   expect(o).toEqual({})
 
-  let a = new Array() // Creates an empty array
+  let a = new Array() // Creates an empty array  let a = []
   expect(a).toEqual([])
 
   let d = new Date() // Current date
@@ -95,10 +96,14 @@ test('object creation using Object.create', () => {
   // using Object.getPrototypeOf()
   let a = {name: 'Tom', age: 10}
   expect(Object.getPrototypeOf(a)).toBe(Object.prototype)
+
+  // Object.prototype object does not have any prototypes.
+  expect(Object.getPrototypeOf(Object.prototype)).toBe(null)
   // lets try to access toString method on a
   expect(a.toString()).toBe('[object Object]')
 
   // Object.create helps creating a new object with a prototype object passed to it
+  // b -> a -> Object.prototype
   let b = Object.create(a)
   expect(Object.getPrototypeOf(b)).toBe(a)
 
@@ -110,4 +115,88 @@ test('object creation using Object.create', () => {
 
   // so if a property is not found in an object, it will be searched in its prototype, until the property is found, or
   // we end up traversing until the root, which is Object.prototype, when it happens, you get undefined.
+  expect(b.name).toBe(a.name)
+
+  a.name = 'John'
+  expect(b.name).toBe('John')
+
+  for (let key in b) {
+    console.log(key)
+  }
+
+  let keys = Object.keys(b)
+  expect(keys).toEqual(['company'])
+})
+
+test('hidden properties using a special namespace called Symbol', () => {
+  let b = {x: 10, y: 25}
+  let values = Object.values(b)
+  expect(values).toEqual([10, 25])
+  let hiddenKey = Symbol('hidden')
+  b[hiddenKey] = 'Some secret'
+  console.log(Object.getOwnPropertySymbols(b))
+  // Symbol keys will not be seen by for..in, Object.keys. Object.values too wont see the corresponding values
+  // But however please understand that Symbol is not a security mechanism
+  // The point of Symbols is not security, but to define a safe extension mechanism for JavaScript objects.
+  // If you get an object from third-party code that you do not control and
+  // need to add some of your own properties to that object but want to be sure that your
+  // properties will not conflict with any properties that may already exist on the object,
+  // you can safely use Symbols as your property names.
+  // If you do this, you can also be confident that the third-party code
+  // will not accidentally alter your symbolically named properties.
+  // (That third-party code could, of course,
+  // use Object.getOwnPropertySymbols() to discover the Symbols you’re using and could then alter or delete your properties.
+  // This is why Symbols are not a security mechanism.)
+  // Ref section
+  expect(b[Symbol('hidden')]).not.toEqual(b[hiddenKey])
+  let keys = Object.keys(b)
+  expect(keys).toEqual(['x', 'y'])
+  expect(values).toEqual([10, 25])
+
+  // Two Symbol invocations with same key will result in different keys only.
+  let hiddenKey1 = Symbol('hidden')
+  expect(hiddenKey).not.toBe(hiddenKey1)
+  b[hiddenKey1] = 'Another extension!'
+
+  // You can get the symbols being used on an object using Object.getOwnPropertySymbols()
+  const allSymbols = Object.getOwnPropertySymbols(b)
+  expect(allSymbols).toEqual([hiddenKey, hiddenKey1])
+})
+
+test('deleting from object', () => {
+  const obj = {x: 10, y: 25, z: 40}
+  expect(Object.keys(obj)).toEqual(['x', 'y', 'z'])
+  delete obj.z
+  expect(Object.keys(obj)).toEqual(['x', 'y'])
+  // Deleting non existent key is a no op
+  delete obj.z // trying to delete z again now has no effect
+  expect(Object.keys(obj)).toEqual(['x', 'y'])
+})
+
+test('shallow copying properties of one or more objects into an object', () => {
+  // Using spread operator
+  let a = {a: 1, b: 2, c: 3}
+  let b = {c: 4, d: 5, e: 6}
+
+  let merged = {...a, ...b} // spread operator creates a shallow copy
+  expect(Object.keys(merged)).toEqual(['a', 'b', 'c', 'd', 'e'])
+
+  let complex1 = {a: {b: 10}}
+  merged = {...merged, ...complex1}
+  expect(merged['a']).toBe(complex1.a) // toBe works, as the copying is shallow here, not deep.
+
+  // if same keys overlap then, the last in the sequence will have effect
+  let c = {a: 15, b: 29}
+
+  let f = {...a, ...b, ...c}
+  expect(f).toEqual({a: 15, b: 29, c: 4, d: 5, e: 6})
+  f = {...f, some: {u: 10, v: 15}}
+  // We can easily create a new shallow copy of an object
+  const shallowF = {...f} // create a shallow copy of f
+  expect(f).not.toBe(shallowF) // f and shalowF are different in memory.
+  expect(f).toEqual(shallowF)
+
+  // the object hierarchy after first level will be same object references..
+  expect(f.some).toBe(shallowF.some)
+  // If you need deep copying of objects, you either need to implement one, or need to use a library such as lodash.
 })
